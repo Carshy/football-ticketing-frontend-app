0e0d651d6c281d7b7482bb14b740195ddaa5110f
ifzyy <johnsonnifemi8@gmail.com>
2022-12-07 15:55:53 +0100

    update match.js

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,25 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMatches } from '../store/matches/matches';
+import Game from './game';
 
 const Match = () => {
   const dispatch = useDispatch();
-  const matches = useSelector((state) => state.matches.matches);
-  console.log(matches);
+  const fixture = useSelector((state) => state.matches.matches);
   useEffect(() => {
     dispatch(fetchMatches());
   }, []);
 
   return (
     <div>
-      {/* {(matches) ? matches.map(match =>{
-      <p key={match.id}>{match.away_team}</p>
-     })
-    :
-    <p>None</p>
-    } */}
+      {fixture.map((match) => (
+        <Game
+          key={match.id}
+          id={match.id}
+          hometeam={match.home_team}
+          awayteam={match.away_team}
+          stadium={match.stadium}
+          location={match.location}
+          price={match.price}
+          date={match.date}
+          photo={match.photo}
+        />
+      ))}
     </div>
-
   );
 };
 
