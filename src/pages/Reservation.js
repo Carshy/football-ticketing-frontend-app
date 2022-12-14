eabe8cb6b2e0ee761ef674b673b821127f412774
Collins <mussocollins@gmail.com>
2022-12-14 05:29:02 +0300

    Add state for reservation

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
+import { userTicket } from '../redux/tickets/tickets';
 import circle from '../assets/circle.svg';
 import '../styles/reservation.scss';
 
 function Reservation() {
+  const [city, setCity] = useState('');
+  const [matchTime, setMatchTime] = useState('');
+  const [matchId, setMatchId] = useState('');
+
+
   return (
     <div className="form-field">
+      <h3 className="reservation-title">Book your Reservation</h3>
       <motion.div
         whileInView={{ scale: [0, 1], opacity: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
@@ -13,17 +21,14 @@ function Reservation() {
       >
         <form>
           <div>
-            <input name="city" placeholder="Enter City" required />
+            <input type="text" name="city" id="city" placeholder="Enter City" required />
           </div>
           <div>
-            <input name="date" placeholder="Match Date" required />
+            <input type="datetime-local" id="datetime" name="datetime" required />
           </div>
-          <select name="framework">
-            <option value="">I am interesting in...</option>
-            <option value="react">React</option>
-            <option value="vue">Vue</option>
-            <option value="angular">Angular</option>
-            <option value="svelte">Svelte</option>
+          <select id="match" name="matchId">
+            <option value="">Select your Match...</option>
+            {}
           </select>
           <input type="submit" value="Book Now" />
         </form>
