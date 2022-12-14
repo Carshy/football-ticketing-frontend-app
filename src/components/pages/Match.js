eda7da9840f7a6f4b4ef00ab136ee02819b406dc
Michael <madetunji1376@gmail.com>
2022-12-14 15:41:44 +0100

    eslint errors corrected

--- src/components/pages/Match.js
+++ src/components/pages/Match.js
@@ -9,6 +9,7 @@ const Match = () => {
   const myRef = useRef(null);
   const dispatch = useDispatch();
   const fixture = useSelector((state) => state.matches.matches);
+  console.log(fixture);
   useEffect(() => {
     dispatch(fetchMatches());
   }, []);
