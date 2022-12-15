1de6a7872a4c35040c1d06be7a2cfca622404d84
Collins <mussocollins@gmail.com>
2022-12-15 16:16:25 +0300

    Add styles for images

--- src/components/pages/Tickets.js
+++ src/components/pages/Tickets.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles/ticketlist.scss';
 import trophy from './images/trophy.jpg';
+import ball from './images/ball.jpg';
 
 const Tickets = (props) => {
   const { id, date, city } = props;
@@ -12,16 +13,20 @@ const Tickets = (props) => {
         <h4>Football Ticket</h4>
       </div>
       <div className="ticket-details">
-        <h3>
-          Location:
-          {' '}
-          {city}
-        </h3>
-        <p>
-          Date:
-          {' '}
-          {date}
-        </p>
+        <img className="ticket-ball" src={ball} alt="ticket trophy" />
+        <div className="ticket-info">
+          <h3>
+            Location:
+            {' '}
+            {city}
+          </h3>
+          <p>
+            Date:
+            {' '}
+            {date}
+          </p>
+        </div>
+        <img className="ticket-ball" src={ball} alt="ticket trophy" />
       </div>
     </div>
   );
