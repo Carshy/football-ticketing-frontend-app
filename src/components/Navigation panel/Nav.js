b9b2dea6160a9b02f2e41bd15de1565233cf6a60
Collins <mussocollins@gmail.com>
2022-12-15 06:29:48 +0300

    Fix conflicts and added missing modules

--- src/components/Navigation panel/Nav.js
+++ src/components/Navigation panel/Nav.js
@@ -10,7 +10,8 @@ import toast, { Toaster } from 'react-hot-toast';
 import Details from '../pages/Details';
 import Match from '../pages/Match';
 import AddMatch from '../pages/add_match';
-// import MyReservations from '../pages/MyReservations';
+import ReservationForm from '../pages/ReservationForm';
+import TicketList from '../pages/TicketList';
 import './styles/navbar.scss';
 import Register from '../pages/Register';
 import Delete from '../pages/Delete';
@@ -96,6 +97,8 @@ function Nav() {
         <Route path="matches/:id" element={<Details />} />
         <Route path="/add_match" element={<AddMatch />} />
         <Route path="/delete_match" element={<Delete />} />
+        <Route path="/TicketList" element={<TicketList />} />
+        <Route path="/ReservationForm" element={<ReservationForm />} />
       </Routes>
     </>
   );
